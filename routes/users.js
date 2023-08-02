0e9f6a7407d65d136ad81b70b64f3ae8608d9ed3
Ritik <shuklaritik731@gmail.com>
2023-08-02 21:31:16 +0530

    setting up our view engine

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require("../controllers/users_controller")
+
+
+
+
+router.get('/profile',usersController.profile);
+
+
+
+
+
+
+
+
+module.exports = router;
